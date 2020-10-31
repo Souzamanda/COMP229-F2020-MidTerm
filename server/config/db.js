0e91cde3bb1e2a80a1b649732e98dafe13268366
Amanda Cordeiro <acsputtomatti@gmail.com>
2020-10-30 21:28:52 -0400

    updated db URI to work with mongodb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://souzamanda:xFXWVQ78RoX3difL@cluster0.q01bu.mongodb.net/books229?retryWrites=true&w=majority"
 };
